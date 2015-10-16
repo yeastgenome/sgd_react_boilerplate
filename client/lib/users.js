5a0a078fb919ff900e9f74d5abefa05a7a4854f2
Ben Hitz <hitz@stanford.edu>
2015-10-16 13:54:37 -0700

    add users.js

---
+++ client/lib/users.js
@@ -0,0 +1,72 @@
+/*
+    A list of users that can be required elsewhere in the application, like this:
+    var users= require("users");
+*/
+
+module.exports = [
+    {
+        name: 'Anna Banana',
+        email: 'ab@bc.com',
+        permissions: 'admin'
+    },
+    {
+        name: 'Carlos Duarte',
+        email: 'cd@bc.com',
+        permissions: 'submitter'
+    },
+    {
+        name: 'Edgar Firenze',
+        email: 'ef@bc.com',
+        permissions: 'submitter'
+    },
+    {
+        name: 'Gloria Hitz',
+        email: 'gh@bc.com',
+        permissions: 'submitter'
+    },
+    {
+        name: 'Irene Jalover',
+        email: 'ij@bc.com',
+        permissions: 'view-only'
+    },
+    {
+        name: 'Kris Logos',
+        email: 'kl@bc.com',
+        permissions: 'view-only'
+    },
+    {
+        name: 'Mark Narc',
+        email: 'mn@bc.com',
+        permissions: 'view-only'
+    },
+    {
+        name: 'Ophilia Pendantic',
+        email: 'op@bc.com',
+        permissions: 'view-only'
+    },
+    {
+        name: 'Quillian Roberts',
+        email: 'qr@bc.com',
+        permissions: 'admin'
+    },
+    {
+        name: 'Steve Towers',
+        email: 'st@bc.com',
+        permissions: 'view-only'
+    },
+    {
+        name: 'Ulysses Vonnegut',
+        email: 'uv@bc.com',
+        permissions: 'view-only'
+    },
+    {
+        name: 'William Xavier',
+        email: 'wx@bc.com',
+        permissions: 'view-only'
+    },
+    {
+        name: 'Yasmina Zerbina',
+        email: 'yz@bc.com',
+        permissions: 'admin'
+    }
+];
