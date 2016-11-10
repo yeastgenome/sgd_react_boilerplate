bc0eac4c59ef2277732a108d719b8f260c6e211b
Travis Sheppard <tshepp@stanford.edu>
2016-11-10 12:16:01 -0800

    remove, simplify, run basic test

--- src/reactApplication.js
+++ src/reactApplication.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, createMemoryHistory } from 'react-router';
 import routes from './routes';
 
 class ReactApp extends Component {
   render() {
+    let _history = (typeof browserHistory === 'object') ? browserHistory : createMemoryHistory('/');
     return (
-      <Router history={browserHistory}>
+      <Router history={_history}>
         {routes}
       </Router>
     );
